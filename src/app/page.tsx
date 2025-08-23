89039f5d7b29cd47914a58bd4f3a0cfd27bd670a
Erick Araújo <araujoerick.dev@gmail.com>
2025-08-22 21:27:13 -0300

    ✨ feat: update home page with products components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
-import Header from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
-      <main>
-        <h1>Home</h1>
+      <main className="flex flex-col gap-5">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full px-5"
+        />
+
+        <ProductList title="Mais vendidos" products={products} />
+
+        <Image
+          src="/banner-02.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full px-5"
+        />
       </main>
     </>
   );
-}
+};
+
+export default Home;
