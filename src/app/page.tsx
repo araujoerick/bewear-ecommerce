73ad3112002c9a996aa08f7ad85273c789519ea6
Erick Araújo <araujoerick.dev@gmail.com>
2025-08-22 21:33:19 -0300

    ✨ feat: add category selector component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/category-selector";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -10,6 +11,7 @@ const Home = async () => {
       variants: true,
     },
   });
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
@@ -25,6 +27,10 @@ const Home = async () => {
         />
 
         <ProductList title="Mais vendidos" products={products} />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
 
         <Image
           src="/banner-02.png"
